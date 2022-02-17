baecb5c9d06f8e60df4dd9d74ce6a8097082c3e1
zhenshan <zhenshan@belmont.tech>
2022-02-17 11:40:13 +0800

    ADD file schema

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from wordcloud_processing import clean_corpus, get_tf_idf, compute_wc, prepare_html_output
 
 
-def create_wordcloud(text_path:"file"):
+def compute(text_path):
     start = datetime.now()
     papers, list_words = clean_corpus(text_path)
     print(f"finish cleaning text: {datetime.now() - start}")
-    
+
     start = datetime.now()
     df, feature_names = get_tf_idf(papers['description'])
     black, pink = compute_wc(df, feature_names)
@@ -16,6 +16,17 @@ def create_wordcloud(text_path:"file"):
 
     return [{"type": "html", "data": html_out}]
 
+def schema():
+    r = [
+        {
+            "id":    "text_path",
+            "type":  "file",
+            "label": "paragraph text path",
+            "props": {}
+        }
+    ]
+
+    return r
 
 # if __name__ == "__main__":
 #     path = '/Users/zhenshanjin/Documents/Belmont/sandy/UtilityDaisies/WordCloud/abstracts.p'
