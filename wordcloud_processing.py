cd64e4719f0fff484019f66fa1e4925caf5b498b
John <johnbt@runbox.com>
2022-03-03 09:56:02 -0700

    takes a df in input

--- wordcloud_processing.py
+++ wordcloud_processing.py
@@ -51,7 +51,8 @@ def clean_corpus(text_path):
         papers = pd.DataFrame([{"id": i, "description": t} for i, t in enumerate(data)])
     else:
         # papers = pd.DataFrame(text_path)
-        papers = text_path
+        data = text_path['title'].tolist()
+        papers = pd.DataFrame([{"id": i, "description": t} for i, t in enumerate(data)])
 
     
     
