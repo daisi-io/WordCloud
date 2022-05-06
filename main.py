2e497a1187ef7b7328f17b1fafef6f3a429515dc
zhenshan <zhenshan@belmont.tech>
2022-05-06 17:27:27 -0500

    add endpoint

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ def compute(texts_path:pd.DataFrame):
         {"type": "image", "label": "black", "data":  {"alt": "Black WordCloud", "src": "data:image/png;base64, " + black}},
         {"type": "image", "label": "pink", "data":  {"alt": "Pink WordCloud", "src": "data:image/png;base64, " + pink}},
     ]
+
+
+def generate_wordcloud_byte(texts_path:pd.DataFrame):
+    print(f"input path: {texts_path}")
+    start = datetime.now()
+    papers, list_words = clean_corpus(texts_path)
+    print(f"finish cleaning text: {datetime.now() - start}")
+
+    start = datetime.now()
+    df, feature_names = get_tf_idf(papers['description'])
+    black, pink = compute_wc(df, feature_names)
+    print(f"finish compute word cloud: {datetime.now() - start}")
+    
+    html_out = prepare_html_output(black, pink)
+
+    return ("data:image/png;base64, " + black, "data:image/png;base64, " + pink)
